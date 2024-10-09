5df6298464424ea70d3125ee6ade6bba165526bc
Viascheslav Bandylo <viacheslav.bandylo@yandex.ru>
2024-10-09 19:03:45 +0400

    Init commit

---
+++ shop_app/serializers.py
@@ -0,0 +1,70 @@
+import re
+from shop_app.models import *
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class SupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    supplier = SupplierSerializer(read_only=True)
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = ProductDetail
+        fields = '__all__'
+
+
+class ProductDetailCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductDetail
+        fields = '__all__'
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    address = AddressSerializer(read_only=True)
+
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        read_only_fields = ['date_joined',]
+
+
+class CustomerCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'address']
+
+    def validate_phone_number(self, value):
+        if not re.match(r'^\d{10,15}$', value):
+            raise serializers.ValidationError('Phone number must be 10-15 digits')
+        return value
