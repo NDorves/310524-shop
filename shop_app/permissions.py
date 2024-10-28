4666d826fc4cbf9f0c319885499f541ea3683e9f
Viascheslav Bandylo <viacheslav.bandylo@yandex.ru>
2024-10-28 15:07:31 +0400

    Add custom permission IsWorkHour

--- shop_app/permissions.py
+++ shop_app/permissions.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework.permissions import BasePermission
 
 
@@ -21,3 +23,10 @@ class IsAdminOrOwner(BasePermission):
             return True
         # Только администратор или владелец может изменять объект
         return request.user.is_staff or obj.owner == request.user
+
+
+class IsWorkHour(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        current_hour = datetime.now().hour
+        # Допустим, рабочие часы с 9 до 18
+        return 9 <= current_hour < 18
